2a9aafec214f2ba40bf59f29e0dfd0b4e80a0f09
Arthur Onoszko <arthur.onoszko@ffcg.se>
2014-01-20 17:34:02 +0100

    Changed name of view controller

--- Snus/MainViewController.m
+++ Snus/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 Arthur Onoszko. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad
 {
